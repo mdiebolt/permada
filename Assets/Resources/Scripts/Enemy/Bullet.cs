89b6e1b11ab53e111aa36ed8fdb78faa54b7f861
Matt Diebolt <mdiebolt@gmail.com>
2013-12-14 17:26:35 -0600

    adding enemy that shoots direction bullets

---
+++ Assets/Resources/Scripts/Enemy/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Enemy {
+  public class Bullet : MonoBehaviour {
+    private Vector3 direction = new Vector3(0, 0, 0); 
+    private float speed = 5.0f;
+
+  	void Update () {
+      var rotation = transform.eulerAngles;
+      rotation.z -= 5.0f;
+       
+      transform.eulerAngles = rotation;
+
+      var position = transform.position;
+
+      position += (direction * speed * Time.deltaTime);
+
+      transform.position = position;
+  	}
+
+    void SetDirection(Vector3 dir) {
+      direction = dir;
+    }
+  }
+}
---
+++ Assets/Resources/Scripts/Enemy/Shoot.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Enemy;
+
+namespace Enemy {
+  public class Shoot : MonoBehaviour {
+    private float elapsedTime; 
+    private GameObject boomerang;
+
+    void Start() {
+      boomerang = Resources.Load<GameObject>("Prefabs/Boomerang");
+    }
+
+    void Update() {
+      elapsedTime += Time.deltaTime;
+
+      if (elapsedTime > 5.0f) {
+        Fire();
+        elapsedTime = 0;
+      }
+  	}
+
+    private void createShot(Vector3 dir) {
+      var obj = Instantiate(boomerang, transform.position, Quaternion.identity) as GameObject;
+      obj.SendMessage("SetDirection", dir);
+    }
+
+    void Fire() {
+      createShot(new Vector3(0, 1, 0));
+      createShot(new Vector3(0, -1, 0));
+      createShot(new Vector3(1, 0, 0));
+      createShot(new Vector3(-1, 0, 0));
+    }
+  }
+}
